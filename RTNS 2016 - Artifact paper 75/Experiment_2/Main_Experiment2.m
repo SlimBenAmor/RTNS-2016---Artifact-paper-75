23534dce0289da4fa629f968a2ca26e9b93c707e
SlimBenAmor <benamorslim19@gmail.com>
2016-10-12 16:27:57 +0200

    Update Main_Experiment2.m

--- RTNS 2016 - Artifact paper 75/Experiment_2/Main_Experiment2.m
+++ RTNS 2016 - Artifact paper 75/Experiment_2/Main_Experiment2.m
@@ -9,18 +9,28 @@ Utilization=0.75;     % system utilization
 DMP=[];
 DMPmax=0;
 
+clc;
+disp('Starting please wait...');
+
 for i=1:NumberOfAnalysedTaskSets
+    disp('generate graph please wait...');
     G = generate_Graph(NumberOfTasks);                    % generate precedence graph
+    disp('generate task please wait...');
     Tau=generate_tasks(NumberOfTasks,NumberOfValues,Utilization);   % generate random task set
+    disp('transform task please wait...');
     T = transformPrecedenceTaskSet ( Tau,G );  % modify timing parameters
+    disp('schedulability test please wait...');
     [dmp,DMPmax]=sporadicSchedTest(T,0.1);     % schedulability test
     DMP=[DMP DMPmax];
+    str=sprintf('%d%% done. Please wait... ',fix(i/NumberOfAnalysedTaskSets*100));
+    disp(str);
 end
 
 HistBars=zeros(1,11);
 for i=1:size(DMP,2)
     HistBars(fix((1-DMP(i))*10)+1)=HistBars(fix((1-DMP(i))*10)+1)+1;
 end
+disp('Finished');
 figure('units','normalized','outerposition',[0.25 0.25 0.5 0.75])
 bar(0:10:100,HistBars);
 xlabel('X = Schedulability Ratio');
