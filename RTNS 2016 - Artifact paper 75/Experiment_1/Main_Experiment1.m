97e33a9184011c0c00267f8aefc9cf25c3e7404f
SlimBenAmor <benamorslim19@gmail.com>
2016-10-12 16:27:00 +0200

    Update Main_Experiment1.m

--- RTNS 2016 - Artifact paper 75/Experiment_1/Main_Experiment1.m
+++ RTNS 2016 - Artifact paper 75/Experiment_1/Main_Experiment1.m
@@ -14,12 +14,22 @@ Utilization=0.75;     % system utilization
 
 DMP=[];
 DMPmax=0;
-    
+
+clc;
+disp('Starting please wait...');
+
 for i=1:NumberOfAnalysedTaskSets
+    disp('generate task please wait...');
     Tau=generate_tasks(NumberOfTasks,NumberOfValues,Utilization);               % generate random task set
+    disp('transform task please wait...');
     T = transformPrecedenceTaskSet ( Tau,G );  % modify timing parameters
+    disp('schedulability test please wait...');
     [dmp,DMPmax]=sporadicSchedTest(T,0.1);     % schedulability test
     DMP=[DMP DMPmax];
+    clc;
+    str=sprintf('%d%% done. Please wait... ',fix(i/NumberOfAnalysedTaskSets*100));
+    disp(str);
 end
+disp('Finished');
 OurTestRatio=(1-DMP')*100
 toc
